94c1c0bf44f7e7b2e2f611b233fef76e2478be77
raja <learnfrommobile@gmail.com>
2023-03-13 20:46:51 +0530

    latest1

--- index.js
+++ index.js
@@ -20,11 +20,13 @@ app.use(express.json());
 app.use(express.static('uploads'))
 
 
+const frontendurl = process.env.FRONTEND_URL;
+
 
 
 const socketIO = require('socket.io')(http, {
     cors: {
-        origin: ['https://socialmediax.netlify.app',"http://localhost:3000"],
+        origin: [frontendurl,"http://localhost:3000"],
         credentials: true
     }
 });
